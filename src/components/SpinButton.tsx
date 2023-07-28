4c65625148a6f7e604e4a3019d019a182b018da1
chsua <csj1919@naver.com>
2023-07-28 15:07:38 +0900

    refactor: 메세지 박스 삭제한 방식으로 수정

--- src/components/SpinButton.tsx
+++ src/components/SpinButton.tsx
@@ -1,24 +1,20 @@
 import React, { useState, MouseEvent } from "react";
 import "./SpinButton.css";
-import Announcement from "./Announcement";
 
 const SpinButton: React.FC = () => {
   const [count, setCount] = useState<number>(0);
   const [isTooltipVisible, setIsTooltipVisible] = useState<boolean>(false);
-  // const [message, setMessage] = useState("");
 
   const increment = () => {
     if (count >= 3) return alert("4인 이상은 불가합니다.");
 
     setCount((prevCount) => prevCount + 1);
-    // setMessage(`${count + 1}명으로 설정되었습니다.`);
   };
 
   const decrement = () => {
     if (count <= 0) return alert("0인 이하는 불가합니다.");
 
     setCount((prevCount) => prevCount - 1);
-    // setMessage(`${count - 1}명으로 설정되었습니다.`);
   };
 
   const toggleTooltip = (event: MouseEvent<HTMLDivElement>) => {
@@ -54,10 +50,11 @@ const SpinButton: React.FC = () => {
           type="text"
           role="spinbutton"
           readOnly
-          disabled
+          // disabled
           className="spinButtonInput"
           value={count}
           aria-label={`현재 성인승객 ${count}명`}
+          aria-disabled="true"
           aria-live="polite"
           aria-readonly="true"
         />
@@ -68,7 +65,6 @@ const SpinButton: React.FC = () => {
         >
           +
         </button>
-        {/* <Announcement>{message}</Announcement> */}
       </div>
     </section>
   );
